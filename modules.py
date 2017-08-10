39d126a0fa1e771990011b9e570b16e47247e90c
Liam Galbraith <liamgalbraith@gmail.com>
2017-08-10 20:25:06 +0100

    Implement SQL for add_component, check_comps

--- modules.py
+++ modules.py
@@ -4,7 +4,6 @@
 Created on Tue Aug  1 23:52:21 2017
 """
 
-# Imports sqlite and sets up the connection
 import sqlite3
 
 file = 'database.db'
@@ -13,23 +12,17 @@ c = conn.cursor()
 
 
 class Module():
-    '''Module objects store all components in dictionary of objects. They can be
-    accessed using the .component[name].'''
 
-    component = {}
-
-    def __init__(self, name, credit, category):
-        self.name = name.lower()
+    def __init__(self, module, credit, category):
+        self.module = module.lower()
         self.credit = credit
         self.category = category
 
-        c.execute('''SELECT name
+        c.execute('''SELECT module
                      FROM Modules
                      WHERE
-                        name = ?
-                     GROUP BY
-                        Name''',
-                 (self.name,)
+                        module = ?''',
+                 (self.module,)
         )
 
         msg = c.fetchone()
@@ -40,7 +33,7 @@ class Module():
 
 
         else:
-            c.execute('''INSERT INTO Modules values (?,?,?)''', (self.name, self.credit, self.category))
+            c.execute('''INSERT INTO Modules values (?,?,?)''', (self.module, self.credit, self.category))
             conn.commit()
 
 
@@ -48,30 +41,33 @@ class Module():
         #This is just testing that it worked - returning the data
         c.execute('''SELECT credits, category
                      FROM Modules
-                     WHERE name = ?''',(self.name,))
+                     WHERE module = ?''',(self.module,))
         print(c.fetchall()[0])
 
     def add_component(self, name, weight, category):
-            self.component[name] = self.Component(name, weight, category)
+            c.execute('''INSERT INTO Components values (?,?,?,?)''', (self.module, name, weight, category,))
+            conn.commit()
 
-    def valid_comps(self):
-        weighting = 0
-        for comp in self.component:
-            weighting += self.component[comp].weight
-        if weighting != 100:
-            raise ValueError('The component weightings do not add up to 100%.')
+            c.execute('''SELECT *
+                         FROM Components
+                         WHERE module = ?''', (self.module,))
+            print(c.fetchall())
 
+    def check_comps(self):
+        c.execute('''SELECT sum(weight)
+                     FROM Components
+                     WHERE
+                        module = ?
+                     GROUP BY
+                        module''', (self.module,))
 
-    # Component class holds info about each aspect of  module
-    class Component():
-        def __init__(self, name, weight, category):
-            self.name = name
-            self.weight = weight
-            self.category = category
-
-        def add_grade(self, grade_type, grade):
-            self.grade_type = grade_type
-            self.grade = grade
+        total = c.fetchone()[0]
+        print(str(total) + "%")
+        # weighting = 0
+        # for comp in self.component:
+        #     weighting += self.component[comp].weight
+        # if weighting != 100:
+        #     raise ValueError('The component weightings do not add up to 100%.')
 
 
 # =============================================================================
@@ -81,17 +77,22 @@ class Module():
 
 module_list={}
 
-def create_module(name, credit, category):
-    module_list[name] = Module(name, credit, category)
+def create_module(module, credit, category):
+    module_list[module] = Module(module, credit, category)
 
 
-# Just tesing it with values - on my editor I can't actually input values
 #example_name = input("Enter the module name: ")
-#example_credit = input("Enter module credits: ")
-#example_category = input("Enter module category: ")
-
 example_name = 'PHY250'
+#example_credit = input("Enter module credits: ")
 example_credit = 25
+#example_category = input("Enter module category: ")
 example_category = 'Exam'
 
+
+
+
+
 create_module(example_name, example_credit, example_category)
+
+# module_list[example_name].add_component('Class Test 1', 80, 'Exam')
+module_list[example_name].check_comps()
