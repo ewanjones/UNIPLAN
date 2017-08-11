14bb096734520202264aba680e81f97d8de9805d
Liam Galbraith <liamgalbraith@gmail.com>
2017-08-11 07:58:59 +0100

    Add change functions, get average...

--- modules.py
+++ modules.py
@@ -13,9 +13,9 @@ c = conn.cursor()
 
 class Module():
 
-    def __init__(self, module, credit, name):
+    def __init__(self, module, credits, name):
         self.module = module.lower()
-        self.credit = credit
+        self.credits = credits
         self.name = name
 
         c.execute('''SELECT module
@@ -31,7 +31,7 @@ class Module():
 
         else:
             c.execute('''INSERT INTO Modules values (?,?,?)''',
-                         (self.module, self.credit, self.name))
+                         (self.module, self.credits, self.name))
             conn.commit()
 
     def add_component(self, name, category, weight, grade=200):
@@ -39,12 +39,22 @@ class Module():
                      (self.module, name, category, weight, grade,))
         conn.commit()
 
+    def get_components(self):
+        c.execute('''SELECT *
+                     FROM Components
+                     WHERE module = ?''',
+                     (self.module,))
+        for comp in c.fetchall():
+            print(comp)
+
     def get_current_grade(self):
         c.execute('''SELECT
                         round(sum(1.0 * (grade * weight) / 100), 2)
                      FROM Components
                      WHERE
-                        module = ?''', (self.module,))
+                        module = ?
+                        AND
+                        grade != 200''', (self.module,))
         return [c.fetchone()[0], self.check_comps()]
 
     def check_comps(self):
@@ -52,11 +62,42 @@ class Module():
                      FROM Components
                      WHERE
                         module = ?
+                        AND
+                        grade != 200
                      GROUP BY
                         module''', (self.module,))
         return c.fetchone()[0]
 
+    def change_module(self, module):
+        c.execute('''UPDATE Modules
+                     SET module = ?
+                     WHERE module = ?''',
+                     (module, self.module,))
+        conn.commit
 
+        self.module = module
+
+    def change_credits(self, credits):
+        c.execute('''UPDATE Modules
+                     SET credits = ?
+                     WHERE name = ?''',
+                     (credits, self.name,))
+        conn.commit
+
+        self.credit = credits
+
+    def change_name(self, name):
+        c.execute('''UPDATE Modules
+                     SET name = ?
+                     WHERE module = ?''',
+                     (name, self.module,))
+        conn.commit
+
+        self.name = name
+
+    def what_do_i_need_to_average(self, desired):
+        working = self.get_current_grade()
+        return round((100 * (desired - working[0]))/(100 - working[1]), 3)
 
 # =============================================================================
 #  MODULE CREATION
@@ -75,6 +116,8 @@ example_name = 'PHY250'.lower()
 example_credit = 25
 #example_category = input("Enter module category: ")
 example_category = 'Exam'
+# desired = input("Desired grade...")
+desired = 70
 
 
 
@@ -85,8 +128,16 @@ create_module(example_name, example_credit, example_category)
 # module_list[example_name].add_component('Essay', 'Coursework', 20, 72)
 # module_list[example_name].add_component('Homework 1', 'Homework', 5, 92)
 # module_list[example_name].add_component('Homework 2', 'Homework', 5, 90)
+# module_list[example_name].add_component('Final Exam', 'Exam',70)
 
 numbers = module_list[example_name].get_current_grade()
 print(example_name.upper(), "current grade: " + str(numbers[0]) + "%\n"
         "Out of an available: " + str(numbers[1]) + "%\n"
         "Average mark: " + str(round(numbers[0] / numbers[1] * 100, 2)) + "%")
+
+module_list[example_name].get_components()
+
+print("You need to get " +
+      str(module_list[example_name].what_do_i_need_to_average(desired))
+      + "%" + " in the remaining components to get " +
+      str(desired) + "% overall.")
