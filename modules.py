65e2228edc0cba1c8d44186007cc1afd1142cd8e
Ewan Jones <ewangjones@btinternet.com>
2017-08-12 11:42:05 +0100

    New modules.py with SQL queries

--- modules.py
+++ modules.py
@@ -1,143 +1,102 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug  1 23:52:21 2017
-"""
-
 import sqlite3
+from collections import namedtuple
+import CONFIG
 
-file = 'database.db'
-conn = sqlite3.connect(file)
+filename = 'database.sql'
+conn = sqlite3.connect(filename)
 c = conn.cursor()
 
 
-class Module():
+def create_tables():
+    c.execute('''CREATE TABLE IF NOT EXISTS modules
+                    (code TEXT NOT NULL PRIMARY KEY, 
+                     name TEXT, 
+                     credits REAL, 
+                     grade REAL, 
+                     classification REAL)''')
 
-    def __init__(self, module, credits, name):
-        self.module = module.lower()
-        self.credits = credits
-        self.name = name
+    c.execute('''CREATE TABLE IF NOT EXISTS components
+                    (code TEXT, 
+                     name TEXT, 
+                     weight REAL, 
+                     category TEXT, 
+                     grade REAL, 
+                     classification TEXT)''')
 
-        c.execute('''SELECT module
-                     FROM Modules
-                     WHERE
-                        module = ?''',
-                 (self.module,)
-        )
+    c.execute('''CREATE TABLE IF NOT EXISTS timetable
+                    (date DATE, 
+                     start_time TIME, 
+                     end_time TIME, 
+                     code TEXT, 
+                     name TEXT, 
+                     credits REAL)''')
+    conn.commit()
 
-        if c.fetchone():
-            print("Module already exists - function here to ask if they want to replace it / look at it / do nothing?")
-            return None
 
-        else:
-            c.execute('''INSERT INTO Modules values (?,?,?)''',
-                         (self.module, self.credits, self.name))
-            conn.commit()
 
-    def add_component(self, name, category, weight, grade=200):
-        c.execute('''INSERT INTO Components values (?,?,?,?,?)''',
-                     (self.module, name, category, weight, grade,))
+def add_module(code, name, credits, grade=None, classification=None):
+    try:
+        c.execute('''INSERT INTO modules VALUES (?, ?, ?, ?, ?)''', (code, name, credits, grade, classification))
         conn.commit()
-
-    def get_components(self):
-        c.execute('''SELECT *
-                     FROM Components
-                     WHERE module = ?''',
-                     (self.module,))
-        for comp in c.fetchall():
-            print(comp)
-
-    def get_current_grade(self):
-        c.execute('''SELECT
-                        round(sum(1.0 * (grade * weight) / 100), 2)
-                     FROM Components
-                     WHERE
-                        module = ?
-                        AND
-                        grade != 200''', (self.module,))
-        return [c.fetchone()[0], self.check_comps()]
-
-    def check_comps(self):
-        c.execute('''SELECT sum(weight)
-                     FROM Components
-                     WHERE
-                        module = ?
-                        AND
-                        grade != 200
-                     GROUP BY
-                        module''', (self.module,))
-        return c.fetchone()[0]
-
-    def change_module(self, module):
-        c.execute('''UPDATE Modules
-                     SET module = ?
-                     WHERE module = ?''',
-                     (module, self.module,))
-        conn.commit
-
-        self.module = module
-
-    def change_credits(self, credits):
-        c.execute('''UPDATE Modules
-                     SET credits = ?
-                     WHERE name = ?''',
-                     (credits, self.name,))
-        conn.commit
-
-        self.credit = credits
-
-    def change_name(self, name):
-        c.execute('''UPDATE Modules
-                     SET name = ?
-                     WHERE module = ?''',
-                     (name, self.module,))
-        conn.commit
-
-        self.name = name
-
-    def what_do_i_need_to_average(self, desired):
-        working = self.get_current_grade()
-        return round((100 * (desired - working[0]))/(100 - working[1]), 3)
-
-# =============================================================================
-#  MODULE CREATION
-# =============================================================================
-
-
-module_list={}
-
-def create_module(module, credit, category):
-    module_list[module] = Module(module, credit, category)
-
-
-#example_name = input("Enter the module name: ")
-example_name = 'PHY250'.lower()
-#example_credit = input("Enter module credits: ")
-example_credit = 25
-#example_category = input("Enter module category: ")
-example_category = 'Exam'
-# desired = input("Desired grade...")
-desired = 70
+    except:
+        print('There is already a module with that name.')
 
 
 
+def edit_module(search, code=None, name=None, credits=None, grade=None, classification=None):
+    current = c.execute('''SELECT * FROM modules WHERE code=?''', (search,)).fetchall()         # Current values
+    args = [code, name, credits, grade, classification]                                         # Updated values
+    params = tuple([item if item else current[0][i] for i, item in enumerate(args)])            # Create tuple of combined
+    params += (search,)
+
+    # Update module in SQL
+    query = '''UPDATE modules
+                SET code=?, name=?, credits=?, grade=?, classification=?
+                WHERE code=?'''
+    c.execute(query, params)
+    conn.commit()
 
 
-create_module(example_name, example_credit, example_category)
+def del_module(code):
+    c.execute('''DELETE FROM modules WHERE code=:code''', {'code': code})
+    conn.commit()
 
-# module_list[example_name].add_component('Essay', 'Coursework', 20, 72)
-# module_list[example_name].add_component('Homework 1', 'Homework', 5, 92)
-# module_list[example_name].add_component('Homework 2', 'Homework', 5, 90)
-# module_list[example_name].add_component('Final Exam', 'Exam',70)
 
-numbers = module_list[example_name].get_current_grade()
-print(example_name.upper(), "current grade: " + str(numbers[0]) + "%\n"
-        "Out of an available: " + str(numbers[1]) + "%\n"
-        "Average mark: " + str(round(numbers[0] / numbers[1] * 100, 2)) + "%")
+def add_component(code, name, category, weight, grade=None, classification=None):
+    try:
+        c.execute('''INSERT INTO modules VALUES (:code, :name, :category, :grade, :classification)''',
+                  {'code': code,
+                   'name': name,
+                   'weight': weight,
+                   'category': category,
+                   'grade': grade,
+                   'classification': classification})
+        conn.commit()
+    except:
+        print('There is already a component with that name.')
 
-module_list[example_name].get_components()
 
-print("You need to get " +
-      str(module_list[example_name].what_do_i_need_to_average(desired))
-      + "%" + " in the remaining components to get " +
-      str(desired) + "% overall.")
+def check_valid():
+    # Check the total credits add up to CONFIG.TOTAL_CREDITS
+    total_credits = c.execute('''SELECT SUM(credits)
+                    FROM modules''').fetchall()
+    print(total_credits)
+    if int(total_credits[0][0]) != CONFIG.TOTAL_CREDITS:
+        print("The total credits do not add up to %d." % CONFIG.TOTAL_CREDITS)
+        return False
+
+    # List of modules
+    modules = c.execute('''SELECT code FROM modules''').fetchall()
+    module_list = [item[0] for item in modules]
+
+    # Check the components add up to 100%
+    for module in module_list:
+        total_comp_weight = c.execute('''SELECT SUM(weight) FROM components WHERE code=?''', (module,))
+        if total_comp_weight != 100:
+            print('The component weights for %s do not add up to 100' % module)
+            return False
+
+    return True
+
+
+create_tables()
